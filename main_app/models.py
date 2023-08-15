6eb8c6d0fbf94ccfac4b2aefc47483002caa747c
arsal0308 <arsal.imran0308@gmail.com>
2023-08-15 10:10:09 -0400

    Photo upload functionality

--- main_app/models.py
+++ main_app/models.py
@@ -81,3 +81,9 @@ class Tour(models.Model):
     class Meta:
         ordering = ['-date']
 
+class Photo(models.Model):
+    url = models.CharField(max_length=200)
+    home = models.ForeignKey(Home, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Photo for home_id: {self.home_id} @{self.url}"
